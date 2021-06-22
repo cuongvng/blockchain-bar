509e937bf95e51be39fbd48e300f834642fc51d9
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-06-22 16:11:34 +0700

    load genesis file

---
+++ database/genesis.go
@@ -0,0 +1,24 @@
+package database
+
+import(
+	"io/ioutil"
+	"encoding/json"
+)
+
+type Genesis struct{
+	Balances map[Account]uint32 `json:"balances`
+}
+
+func LoadGenesis(path string) (Genesis, error){
+	content, err := ioutil.ReadFile(path)
+	if err != nil{
+		return Genesis{}, err
+	}
+
+	var g Genesis;
+	err = json.Unmarshal(content, &g)
+	if err != nil{
+		return Genesis{}, err
+	}
+	return g, nil
+}
