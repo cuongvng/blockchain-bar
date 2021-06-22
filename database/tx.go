3afd48f59d9ab666774128af0b9904275780c3a1
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-06-22 16:11:59 +0700

    define struct for transaction

---
+++ database/tx.go
@@ -0,0 +1,18 @@
+package database
+
+type Account string
+
+type Tx struct{
+	From Account `json:"from"`
+	To Account `json:"to`
+	Value uint32 `json:"value`
+	Data string `json:"data`
+}
+
+func NewTx(from Account, to Account, value uint32, data string) Tx{
+	return Tx{from, to , value, data}
+}
+
+func (tx Tx) IsReward() bool{
+	return tx.Data == "reward"
+}
