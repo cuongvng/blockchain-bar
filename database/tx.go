d18cafdb8f435149510f3b75234eca46284a6535
Cuong V. Nguyen <nguyencuongcl1215@gmail.com>
2021-06-22 16:45:10 +0700

    add missing quotes

--- database/tx.go
+++ database/tx.go
@@ -4,9 +4,9 @@ type Account string
 
 type Tx struct{
 	From Account `json:"from"`
-	To Account `json:"to`
-	Value uint32 `json:"value`
-	Data string `json:"data`
+	To Account `json:"to"`
+	Value uint32 `json:"value"`
+	Data string `json:"data"`
 }
 
 func NewTx(from Account, to Account, value uint32, data string) Tx{
